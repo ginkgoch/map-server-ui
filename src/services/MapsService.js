607eba00ecdaee66cedd9ff2ab2bf986a08cd428
Ginkgoch <ginkgoch@outlook.com>
2019-09-23 20:57:33 +0800

    228: Highlight features by click.

--- src/services/MapsService.js
+++ src/services/MapsService.js
@@ -34,8 +34,8 @@ export class MapsService {
     static async deleteMapByID(id) {
         const response = await Requests.delete(`/maps/${id}`);
         return response;
-    } 
-    
+    }
+
     /**
     * 
     * @param {*} layerID 
@@ -43,38 +43,55 @@ export class MapsService {
     * @param {*} mapID 
     * @param {fields: string, from: number, limit: number} filter
     */
-   static async getProperties(layerID, groupID, mapID, filter = undefined) {
-       const response = await Requests.get(`/maps/${mapID}/groups/${groupID}/layers/${layerID}/properties`, filter);
-       return response;
-   }
+    static async getProperties(layerID, groupID, mapID, filter = undefined) {
+        const response = await Requests.get(`/maps/${mapID}/groups/${groupID}/layers/${layerID}/properties`, filter);
+        return response;
+    }
 
-   /**
-    * 
-    * @param {*} layerID 
-    * @param {*} groupID 
-    * @param {*} mapID 
-    * @param {fields: string, types: string} filter 
-    */
-   static async getFields(layerID, groupID, mapID, filter = undefined) {
-       const response = await Requests.get(`/maps/${mapID}/groups/${groupID}/layers/${layerID}/fields`, filter);
-       return response;
-   }
+    /**
+     * 
+     * @param {*} layerID 
+     * @param {*} groupID 
+     * @param {*} mapID 
+     * @param {fields: string, types: string} filter 
+     */
+    static async getFields(layerID, groupID, mapID, filter = undefined) {
+        const response = await Requests.get(`/maps/${mapID}/groups/${groupID}/layers/${layerID}/fields`, filter);
+        return response;
+    }
 
-   static async getPropertyByField(field, layerID, groupID, mapID) {
-       const response = await Requests.get(`/maps/${mapID}/groups/${groupID}/layers/${layerID}/properties/${field}`);
-       return response;
-   }
+    static async getPropertyByField(field, layerID, groupID, mapID) {
+        const response = await Requests.get(`/maps/${mapID}/groups/${groupID}/layers/${layerID}/properties/${field}`);
+        return response;
+    }
 
-   /**
-    * 
-    * @param {*} field 
-    * @param {*} layerID 
-    * @param {*} groupID 
-    * @param {*} mapID 
-    * @param {string[]} aggregators Options are min, max, avg, count and distinct.
-    */
-   static async getAggregatedPropertyByField(field, layerID, groupID, mapID, aggregators) {
-       const response = await Requests.get(`/maps/${mapID}/groups/${groupID}/layers/${layerID}/properties/${field}`, { aggregators });
-       return response;
-   }
+    /**
+     * 
+     * @param {*} field 
+     * @param {*} layerID 
+     * @param {*} groupID 
+     * @param {*} mapID 
+     * @param {string[]} aggregators Options are min, max, avg, count and distinct.
+     */
+    static async getAggregatedPropertyByField(field, layerID, groupID, mapID, aggregators) {
+        const response = await Requests.get(`/maps/${mapID}/groups/${groupID}/layers/${layerID}/properties/${field}`, { aggregators });
+        return response;
+    }
+
+    /**
+     * 
+     * @param {number[]} geom 2 numbers means point [x, y], 4 numbers means envelope [minx, miny, maxx, maxy].
+     * @param {string} geomSrs Geometry srs associated. 
+     * @param {number} level Zoom level.
+     * @param {number | string} mapID Map ID.
+     * @param {string} outSrs The output geometry SRS.
+     * @param {boolean} simplify Whether simplify the geometry based on rendering resolution. It can efficiently reduce the size transfer through network. Default value is true.
+     */
+    static async getIntersection(geom, geomSrs, level, mapID, outSrs, simplify = true) {
+        const response = await Requests.get(`/maps/${mapID}/query/intersection`, {
+            geom, geomSrs, level, outSrs, simplify
+        });
+
+        return response;
+    }
 }
