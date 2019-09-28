75f975a2b330fac00608342841fa1d3de9360a58
Ginkgoch <ginkgoch@outlook.com>
2019-09-28 12:16:11 +0800

    237: Add logo image.

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -4,7 +4,7 @@ import {Avatar} from 'antd';
 export default () => {
     return (
         <h1 style={{display: "inline-block"}}>
-            <Avatar size="large" icon="user"></Avatar>
+            <Avatar size="large" icon="user" src="/images/logo.png"></Avatar>
             <span style={{ "marginLeft": "14px" }}>Ginkgoch</span>
         </h1>
     );
