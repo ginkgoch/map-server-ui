522ca98df7553013cef0c28c79c30d1e9bb636d0
Ginkgoch <ginkgoch@outlook.com>
2020-01-22 11:10:57 +0800

    297: Fixed the server entry point issue.

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
 	yarn build
-	docker build -t ginkgoch/map-server-ui:v1 .
+	docker build -t ginkgoch/map-server-ui:1.0.4 .
--- src/shared/Config.js
+++ src/shared/Config.js
@@ -1,6 +1,11 @@
 export const Config = {
-    serviceRoot: 'http://localhost:3000',
+    serviceRoot: getHostEntryPoint(),
     serviceUrl: path => {
         return [Config.serviceRoot.replace(/\/$/i, ''), path.replace(/^\//i, '')].join('/');
     }
+}
+
+function getHostEntryPoint() {
+    let location = window.location;
+    return `${location.protocol}//${location.hostname}:3000`;
 }
